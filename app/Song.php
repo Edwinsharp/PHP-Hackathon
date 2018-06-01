036ef24a59c27759ee5242a3f3fe2b6fdf0dafc1
“Edwin <edwin.ishimwe@ymail.com>
2018-06-01 14:18:10 +0200

    updated modal

--- app/Song.php
+++ app/Song.php
@@ -37,5 +37,72 @@ class Song
 
         // find the last inserted id and update this object's property
         $this->id = db::getConnection()->lastInsertId();
+
     }
+    public function update()
+    {
+        $query = "
+            UPDATE `songs`
+            SET `name` = ?,
+                `code` = ?,
+                `description` = ?,
+                `author` = ?,
+            WHERE `id` = ?
+        ";
+        $values = [
+            $this->name,
+            $this->code,
+            $this->description,
+            $this->author,
+            $this->id
+        ];
+        db::query($query, $values);
+    }
+    public function delete()
+    {
+        $query = "
+            DELETE
+            FROM `songs`
+            WHERE `id` = ?
+        ";
+        $values = [
+            $this->id
+        ];
+        db::query($query, $values);
+        $this->id = null;
+    }
+    public function save()
+    {
+        if ($this->id) {
+            $this->update();
+        } else {
+            $this->insert();
+        }
+    }
+    public static function find($id)
+    {
+        $query = "
+            SELECT *
+            FROM `songs`
+            WHERE `id` = ?
+        ";
+        $statement = db::query($query, [
+            $id
+        ]);
+        // set the statement to fetch objects of this class
+        $statement->setFetchMode(PDO::FETCH_CLASS, static::class);
+        // fetch and return the first result
+        return $statement->fetch();
+    }
+    public static function select($query, $values = [])
+    {
+        // run the query, get the statement
+        $statement = db::query($query, $values);
+        // set the statement to fetch objects of this class
+        $statement->setFetchMode(PDO::FETCH_CLASS, static::class);
+        // fetch and return all of the results
+        return $statement->fetchAll();
+    }
+    
 }
+
