8ec1ccac43aa43aab79bea0c931aa5481aa70d65
Enakelemichael <enakelemichael@yahoo.com>
2018-06-01 11:36:26 +0200

    done

---
+++ resources/views/insertsong.php
@@ -0,0 +1,26 @@
+<?php
+
+//name: short string;
+//code: short string;
+//description: long string;
+//author: short string,
+
+?>
+
+<form action="">
+  Name:<br>
+  <input type="text" name="name" value="">
+  <br>
+  Code:<br>
+  <input type="text" name="code" value="">
+  <br><br>
+  
+  Description:<br>
+  <textarea type="text" name="description" rows="4" cols="50"></textarea><br><br>
+  
+   Author:<br>
+  <input type="text" name="author" value="">
+  <br><br>
+  
+  <input type="submit" value="Submit">
+</form> 
