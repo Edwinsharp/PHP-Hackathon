7fcd49616738c583afc3c04c05ffe86a9a460a5e
mmiikkii94 <d.miki3@gmail.com>
2018-06-01 14:12:12 +0200

    Submition

--- app/Http/controllers/InsertsongController.php
+++ app/Http/controllers/InsertsongController.php
@@ -3,42 +3,48 @@
 namespace app\Http\controllers;
 
 use \polakjan\mvc\db;
+use app\Song;
 
 class InsertsongController
 {
     public function index()
     {
         
-        // if (!empty($_GET['id'])) {
-        //     // retrieve existing SONG from database
-        // } else {
-        //     // create empty SONG
-        // }
+         if (!empty($_GET['id'])) {
+             // retrieve existing SONG from database
+                
+         } else {
+             // create empty SONG
+            $song = new Song;
+            
+         }
          
-        // // if the form was submitted
-        // if ($_POST) {
-        //     // load the submitted information into the SONG
+         // if the form was submitted
+        if ($_POST) {
+             // load the submitted information into the SONG
+             $song->name = $_POST["name"];
+             $song->code = $_POST["code"];
+             $song->description = $_POST["description"];
+             $song->author = $_POST["author"];
+
+            var_dump($_POST);
+             // validate the data in SONG
+            $data_is_valid = true;
+            if (!$song->name) {
+                $data_is_valid = false;
+            }
+            if (!$song->code) {
+                $data_is_valid = false;
+            }
          
-        //     // validate the data in SONG
-        //     $data_is_valid = true;
-        //     if (!$song->name) {
-        //         $data_is_valid = false;
-        //     }
-        //     if (!$song->code) {
-        //         $data_is_valid = false;
-        //     }
-         
-        //     // if validation did not encounter any errors
-        //     if ($data_is_valid) {
-        //         // save the data
-        //         $query = "
-        //             INSERT INTO ...
-        //         "; // etc.
-         
-        //         // redirect (ideally to the edit page of the inserted song)
-        //         header('Location: /some/url.php');
-        //     }
-        // }
+            // if validation did not encounter any errors
+            if ($data_is_valid) {
+                // save the data
+               $song -> insert();
+                // redirect (ideally to the edit page of the inserted song)
+               header('Location: ?page=insertsong');
+            }
+        }
 
 
         // set the content
