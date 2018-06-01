3442ee27b9ba26209c768d5b7788c57d5b10c9d5
mmiikkii94 <d.miki3@gmail.com>
2018-06-01 11:59:58 +0200

    i

--- app/Http/controllers/InsertsongController.php
+++ app/Http/controllers/InsertsongController.php
@@ -9,36 +9,36 @@ class InsertsongController
     public function index()
     {
         
-        if (!empty($_GET['id'])) {
-            // retrieve existing SONG from database
-        } else {
-            // create empty SONG
-        }
+        // if (!empty($_GET['id'])) {
+        //     // retrieve existing SONG from database
+        // } else {
+        //     // create empty SONG
+        // }
          
-        // if the form was submitted
-        if ($_POST) {
-            // load the submitted information into the SONG
+        // // if the form was submitted
+        // if ($_POST) {
+        //     // load the submitted information into the SONG
          
-            // validate the data in SONG
-            $data_is_valid = true;
-            if (!$song->name) {
-                $data_is_valid = false;
-            }
-            if (!$song->code) {
-                $data_is_valid = false;
-            }
+        //     // validate the data in SONG
+        //     $data_is_valid = true;
+        //     if (!$song->name) {
+        //         $data_is_valid = false;
+        //     }
+        //     if (!$song->code) {
+        //         $data_is_valid = false;
+        //     }
          
-            // if validation did not encounter any errors
-            if ($data_is_valid) {
-                // save the data
-                $query = "
-                    INSERT INTO ...
-                "; // etc.
+        //     // if validation did not encounter any errors
+        //     if ($data_is_valid) {
+        //         // save the data
+        //         $query = "
+        //             INSERT INTO ...
+        //         "; // etc.
          
-                // redirect (ideally to the edit page of the inserted song)
-                header('Location: /some/url.php');
-            }
-        }
+        //         // redirect (ideally to the edit page of the inserted song)
+        //         header('Location: /some/url.php');
+        //     }
+        // }
 
 
         // set the content
